094d225cf19041ba48ce1a5e21021eb0327408ed
mhoshi-vm <machi.hoshino@broadcom.com>
2025-05-29 22:07:29 +0900

    Update to access behind a loadbalancer

--- lecture-3/receiver/src/main/java/com/example/receiver/ReceiverConfiguration.java
+++ lecture-3/receiver/src/main/java/com/example/receiver/ReceiverConfiguration.java
@@ -1,11 +1,17 @@
 package com.example.receiver;
 
+import com.rabbitmq.stream.Address;
 import com.rabbitmq.stream.Environment;
 import com.rabbitmq.stream.OffsetSpecification;
-import org.springframework.amqp.core.*;
+//import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.boot.autoconfigure.amqp.EnvironmentBuilderCustomizer;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.rabbit.stream.config.StreamRabbitListenerContainerFactory;
@@ -23,6 +29,14 @@ class ReceiverConfiguration {
             rabbitAdmin.declareBinding(BindingBuilder.bind(new Queue("stream1")).to(new FanoutExchange("handson")));
             rabbitAdmin.initialize();
         });
+    }
+
+    @Bean
+    EnvironmentBuilderCustomizer environmentBuilderCustomizer(RabbitProperties rabbitProperties) {
+        RabbitProperties.Stream rabbitStreamProperties = rabbitProperties.getStream();
+        Address entryPoint = new Address(rabbitStreamProperties.getHost(), rabbitStreamProperties.getPort());
+        return builder -> builder
+                .addressResolver(address -> entryPoint );
     }
 
     @Bean
